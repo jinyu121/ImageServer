92d25b20ffae9fc0b0c6cf365a1fe0b32f6ffa05
Yu Hao <jinyu121@gmail.com>
2021-04-04 18:05:08 +0800

    Work with static file URLs

--- main.go
+++ main.go
@@ -5,24 +5,31 @@ import (
 	"flag"
 	"fmt"
 	"html/template"
+	"io/fs"
 	"log"
 	"net/http"
 )
 
 var (
-	//go:embed web
-	fs embed.FS
-
-	port = flag.Int("port", 9420, "Listen port")
+	//go:embed web/image web/static
+	staticFiles embed.FS
+	//go:embed web/template
+	templateFiles embed.FS
+	port          = flag.Int("port", 9420, "Listen port")
 )
 
 func index(w http.ResponseWriter, req *http.Request) {
-	t, _ := template.ParseFS(fs, "web/template/index.tmpl", "web/template/base.tmpl")
+	t, _ := template.ParseFS(templateFiles, "web/template/index.tmpl", "web/template/base.tmpl")
 	t.Execute(w, nil)
 }
 
 func main() {
 	flag.Parse()
+
 	http.HandleFunc("/", index)
+
+	staticFilesClean, _ := fs.Sub(staticFiles, "web")
+	http.HandleFunc("/_/", http.StripPrefix("/_/", http.FileServer(http.FS(staticFilesClean))).ServeHTTP)
+
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("[::]:%d", *port), nil))
 }
