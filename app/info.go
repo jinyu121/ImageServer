35a5e48639108950fee3a8551f94f2fa5c9453d0
Yu Hao <jinyu121@gmail.com>
2021-04-05 16:28:15 +0800

    Info

---
+++ app/info.go
@@ -0,0 +1,7 @@
+package app
+
+const (
+	INFO_CODENAME string = "ImageServer"
+	INFO_VERSION  string = "0.0.1"
+	INFO_BUILD    string = "20210101"
+)
--- info/info.go
+++
@@ -1,7 +0,0 @@
-package info
-
-var (
-	CodeName string = "ImageServer"
-	Version  string = "0.0.1"
-	Build    string = "20210101"
-)
